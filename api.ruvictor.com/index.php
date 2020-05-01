d9f6ec600a6fd8c1611cf1f7fbb8afa2cbcbb7c1
AimanAlmureish <aiman.almureish@gmail.com>
2020-05-01 17:15:47 +0300

    Update index.php

--- api.ruvictor.com/index.php
+++ api.ruvictor.com/index.php
@@ -4,7 +4,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // get refferer server
-if($_SERVER['HTTP_REFERER'] === "http://snailsend.herokuapp.com/"){
+if($_SERVER['HTTP_REFERER'] === "https://snailsend.herokuapp.com/"){
     // extract the data from $_POST
     $name = isset($_GET['name']) ? $_GET['name'] : null;
     $message = isset($_GET['message']) ? $_GET['message'] : null;
